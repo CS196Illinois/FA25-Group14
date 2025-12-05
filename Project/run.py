c9dea17cef7a758535b8400d9572324e14058c61
BobJr23 <maandrew2007@gmail.com>
2025-12-04 21:29:35 -0600

    fix bug

--- Project/run.py
+++ Project/run.py
@@ -1,12 +1,10 @@
 from app import create_app
 import os
-from dotenv import load_dotenv
 
 # Load environment variables from .env file
-load_dotenv()
 
 app = create_app()
 
 if __name__ == '__main__':
-    debug_mode = os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
-    app.run(debug=debug_mode, use_reloader=False)
+    debug_mode = False
+    app.run(debug=debug_mode)
